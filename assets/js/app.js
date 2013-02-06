e177fe6de8d9f4d479f99cb4918fbf33cbdfeabf
Axel Sears <axel@ambient.ly>
2013-02-06 13:36:06 -0800

    building template for instance pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,13 @@
         }); 
       }
       // Close messages
-      $('ul#messages > li > a.close').live('click', function() {$(this).parent().remove();});
+      $('ul#messages > li > a.close').live('click', function() {
+        if($('ul#messages > li > a.close').length == 1){
+          $(this).parents('#messages').remove();
+        } else {
+          $(this).parent().remove();
+        }
+      });
       // Profile menu
       $('header > div > div#profile').live('click', function(event) {
         event.stopPropagation();
@@ -39,6 +45,8 @@
           });
           if(!$(this).hasClass('active')){
             $(this).addClass('active');
+          } else {
+            $(this).removeClass('active');
           }
         });
       }
@@ -55,11 +63,16 @@
       messages : function(type,title,msg) {
         if(!title){title=type}
         // Check for messages container
-        if(!$('section > ul#messages').length){
-          $('body > section').prepend('<ul id="messages"></ul>');
+        if(!$('ul#messages').length){
+          if($('div#content.panel').length){
+            var contentArea = $('body > section > div#content.panel > div').first();
+          } else {
+            var contentArea = $('body > section');
+          }
+          contentArea.prepend('<ul id="messages"></ul>');
         }
         // Create message
-        $('body > section > ul#messages').prepend('<li class="'+type+'"><span>'+title+'</span>'+msg+'<a class="close"></a></span>');
+        $('body section ul#messages').prepend('<li class="'+type+'"><span>'+title+'</span>'+msg+'<a class="close"></a></span>');
         $('html, body').animate({scrollTop:0}, 'medium');
       },
       

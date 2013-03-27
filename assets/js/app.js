2748fccb004498d54062ee63c4734e476ac7f854
Axel Sears <axel@ambient.ly>
2013-03-27 08:57:29 -0700

    action menus

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ var strings = {
   client : {
     init : function() {
       if(!$('header').html().length) strings.client.include();
+      strings.events.clicks();
     },
     loaded : function() {
       if($('body').hasClass('loading')) $('body').removeClass('loading');
@@ -24,6 +25,32 @@ var strings = {
         container.prepend('<ul id="messages"></ul>');
       }
       $('body section ul#messages').prepend('<li class="'+type+'"><span>'+title+'</span>'+message+'<a class="close"></a></span>');
+    },
+    actionmenu : function() {
+      $('ul.action-menu').live('click',function(event) {
+        event.stopPropagation();
+        var align = ($(this).outerWidth() - $(this).children('span').outerWidth()) * 0.5;
+        if($(this).attr('data-width')){
+          $(this).children('span').width($(this).attr('data-width'));
+          align = 0;
+        }
+        $(this).children('span').css('right',align);
+        $(this).toggleClass('active');
+      });
+      $('body').click(function() {
+        $('ul.action-menu.active').removeClass('active');
+      });
+    }
+  },
+  
+  events : {
+    clicks : function() {
+      // Close message boxes
+      $('ul#messages li a.close').live('click', function() { $(this).parent().remove() });
+      // Tooltips
+      $('.tooltip').tooltip({ position: { my: "left+2 top+12", at: "left top+12" } });
+      // Action! Menus
+      if($('ul.action-menu').length) strings.ui.actionmenu();
     }
   }
   
